3b5044fdcab995eba3fb638d50b02890cf41b4a1
elbjek <lenabjekovic@hotmaill.com>
2018-06-02 19:43:56 +0200

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="assets/pc-monitor.png" alt="">
+    <loginPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import loginPage from './components/loginPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    loginPage
   }
 }
 </script>
 
 <style>
+body{
+background: linear-gradient(0deg, rgba(51,78,145,1) 0%, rgba(143,224,230,1) 100%);
+background-repeat: no-repeat;
+height:100vh;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:white;
+  padding-top: 25%;
+  font-weight: bolder;
+
 }
 </style>
---
+++ src/components/loginPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+   hello, its me
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
