179dcf2706150207ea234ab443799a2c6557d61f
elbjek <lenabjekovic@hotmaill.com>
2018-06-03 00:21:44 +0200

    added css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <img src="./assets/pc-monitor.png">
-    <loginPage msg="Welcome to Your Vue.js App"/>
+    <loginPage msg="Welcome!"/>
   </div>
 </template>
 
@@ -10,6 +10,7 @@ import loginPage from './components/loginPage.vue'
 
 export default {
   name: 'app',
+ 
   components: {
     loginPage
   }
@@ -26,12 +27,17 @@ height:100vh;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color:white;
   padding-top: 20%;
   font-weight: bolder;
+  text-align: center;
   img{
-    opacity:0.7;
+    // opacity:0.7;
+    text-align: center;
+    margin:0 auto;
+    align-self: center;
+    height:50px;
+    // padding-bottom: 20px;
   }
 }
 </style>
--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -1,127 +1,135 @@
 <template>
-  <div class="hello">
-
-    <form action="" id="form" @submit.prevent="validation">
-
-     <label for="">
-        <input type="text" v-model="email" placeholder="Username/Email">
-        <a href="#" @click="showPass()"> <img src="../assets/checkmark.png" alt=""></a>
-        
-     </label>
-      <p>{{emailError}}</p>
-     <label for=""> 
-        <input :type="passtype" v-model="password" placeholder="Password"> 
-
-        <a href="#" @click="showPass()"> <img src="../assets/eye-open.png" alt=""></a>
-     </label>
-    <p>{{passError}} </p>
-
-  <button type="submit">Login</button>
+  <div  >
+    <h1>{{welcomeMsg}}</h1>
+    <form action=""  @submit.prevent="validation">
+        <div >
+          <label for="email">
+          <input type="text"  name="email" v-model="email" placeholder="Username/Email">
+          </label>
+          <p>{{emailError}}</p>
+        </div>
+        <div >
+          <label for="password"> 
+          <input :type="passtype" name="password" @click="showIcon()" v-model="password" placeholder="Password"> 
+          <a href="#" v-if="show" @click="togglePassword()"> <img src="../assets/eye-open.png" alt=""></a>
+          </label>
+          <p>{{passError}} </p>
+          <button  type="submit">Login</button>
+        </div>
     </form>
-
   </div>
 </template>
-
 <script>
-export default {
-  name: 'loginPage',
-  data(){
-
-    return{
-      show:false,
-      passtype:"password",
-      password:"",
-      email:"",
-      emailError:"",
-      passError:""
-    }
-  },
-  methods:{
-    showPass(){
-      if(this.passtype=="password"){
-        this.passtype = "text"
-      } else{
-        this.passtype="password"
-      }
-    },
-    regEx(){
-      var regularExpresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-       return regularExpresion.test(this.email);   
-    },
-    validation(){
+   export default {
+     name: 'loginPage',
+     data(){
    
-      // console.log("im here",this.password, this.email);
-      if(!this.regEx(this.email) ){
-      this.emailError = "E-mail doesn't exist."
-      } else if(this.email == ""){
-      this.emailError = "E-mail is required."
-      }
-       if(this.password == ""){
-        this.passError = "Password is required."
-        // console.log(this.password)
-      } else if (this.password.length < 3){
-        this.passError = "Password needs to be at least 3 characters long."
-        console.log(this.password)
-        
-      }
+       return{
+         show:false,
+         passtype:"password",
+         password:"",
+         email:"",
+         emailError:"",
+         passError:"",
 
-    }
-  }
+       }
+     },
+     methods:{
+       showIcon(){
+         this.show = true;
+       },
+       togglePassword(){
+         this.show=true;
+         if(this.passtype=="password"){
+           this.passtype = "text"
+         } else{
+           this.passtype="password"
+         }
+       },
+       regEx(){
+         var regularExpresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return regularExpresion.test(this.email);   
+       },
+       validation(){
+      
+         // console.log("im here",this.password, this.email);
+         if(this.email == ""){
+         this.emailError = "E-mail is required."
+         }
+         else if(!this.regEx(this.email) ){
+         this.emailError = "Please enter a valid e-mail."
+         }
+          
+          if(this.password == ""){
+           this.passError = "Password is required."
 
-}
+         } else if (this.password.length < 3){
+           this.passError = "Password needs to be at least 3 characters long."
+         }
+         else{
+           this.password=""
+           this.email=""
+         }
+   
+       }
+     }
+   
+   }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='scss'>
 
-  #form{
-  display:flex;
-  margin: 0 auto;
-  width:50%;
-  // justify-content: flex-start;
-  flex-direction: column;
+     form{
+        display:flex;
+        flex-direction: column;
+        justify-content: space-between;
+       align-items: center;
+       width:30%;
+       margin:0 auto;
 
-  label{
+     label{
+        display: flex;
+        align-items: center;  
+        position: relative;
 
-    display: flex;
-    justify-content: center;
-    align-items: center
-  }
-  input{
-    background-color: rgba($color: #ffffff, $alpha: 0.1);
-    padding:10px;
-    width:35%;
-    margin:5px;
-    border:3px solid   rgba($color: #ffffff, $alpha: 0.8);
-    border-radius: 50px;
-    cursor: pointer; 
-    position: relative;
-    &:hover{
-      border:3px solid   rgba($color: #ffffff, $alpha: 1);
-    }
-
-  }
-img{
-  height:20px;
-  //  position: absolute;
-  // right: 0;
-}
-
-
-  ::placeholder{
-    color:#222222;
-    font-weight: bolder;
+       input{
+         background-color: rgba(#ffffff, 0.2);
+         border:3px solid rgba(#ffffff, 0.8);
+         border-radius: 10px;
+        padding:10px 100px 10px 5px;
+        font-weight: bold;
+      &:hover{
+            background-color: rgba(#ffffff, 0.4);
+            border:3px solid rgba(#ffffff, 1);
+        
+         }
+       }
+      ::placeholder{
+        color:white;
+        // font-weight: ;
       }
-    
-  
-  button{
-    // width:100px;
-    margin:0 auto;
-    background-color: rgba(51,78,145,1);
-    border:1px solid white;
-    padding:15px 50px;
+      img{
+        height:15px !important;
+        opacity: 1 !important; 
+        position:absolute;
+        left:150px;
+        top:5px;
+        transform: translate(-200,0)
+      }
+     }
+      button{
+           background-color:  rgba(#ffffff,1);
+           border:none;
+           padding:10px 23px;
+           border-radius: 10px;
+           color:rgba(51,78,145,1) ;
+           margin-top: 10px;
+         }
+         p{
+           font-size: 15px;
+         }
+  }
+         
 
-}
-
-}
+   
 </style>
